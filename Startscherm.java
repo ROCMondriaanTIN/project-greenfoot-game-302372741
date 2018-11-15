c4b8ad9e995baae3a315758201fb21b07555e074
302372741 <44892186+302372741@users.noreply.github.com>
2018-11-15 12:23:08 +0100

    Lokale map verplaatst naar de Git depository.

---
+++ Startscherm.java
@@ -0,0 +1,24 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Startscherm here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Startscherm extends Actor
+{
+    /**
+     * Act - do whatever the Startscherm wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        prepare();
+    }    
+    private void prepare()
+    {
+        Startscherm Start = new Startscherm();
+        addObject(Start, ,);
+    }
+}
